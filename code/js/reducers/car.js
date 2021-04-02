f073f8e032457929dbf5d8388f9ec55aff9c3443
OLEKSANDR <68386153+Alex21777now@users.noreply.github.com>
2021-04-02 15:26:23 +0300

    React-REDUX-Avtomobili
    React-REDUX-Avtomobili

---
+++ code/js/reducers/car.js
@@ -0,0 +1,28 @@
+export default function () {
+  return [
+    {
+      id: 1,
+      name: "Audi",
+      speed: 234.45,
+      weight: 1.4,
+      desc: "R8 это замечательный спортивный автомобиль!",
+      img: "https://lh3.googleusercontent.com/proxy/6nnoaglp7REWCFEIrPP9878eq2BNbKRqr78E4MfIhT9NwG8rFvJBel4Ea2ozdbHt6amWhyQW8xFEU65qg9h5IGJ5H6s"
+    },
+    {
+      id: 2,
+      name: "BMW",
+      speed: 314.5,
+      weight: 1.2,
+      desc: "Автомобили с отметкой 'M' всегда были быстрее!",
+      img: "https://avatars.mds.yandex.net/get-verba/787013/2a00000160984840410e310d545cdba95902/cattouchret"
+    },
+    {
+      id: 3,
+      name: "Mercedes",
+      speed: 286.1,
+      weight: 1.35,
+      desc: "Mercedes отличается не только скоростью, но также комфортом",
+      img: "https://cdnimg.rg.ru/img/content/183/62/20/53_d_850.jpg"
+    }
+  ]
+}
